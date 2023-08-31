2a168ae53b631a2a8022c44a6efbc567c621b1e3
Istiaq Ahmed <2021462@iub.edu.bd>
2023-09-01 01:53:09 +0600

    removed dark mode

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="dark:bg-gradient-to-t dark:to-gray-800 dark:from-primary bg-gradient-to-t from-primary to-neutral">
+    <Html lang="en" className="bg-gradient-to-t from-primary to-neutral">
       <Head>
         <link rel="icon" href="/logo.png" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"/>
