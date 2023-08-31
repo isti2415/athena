a23a79340ac239cd477fa7fffe44cfd43b05cb6e
Istiaq Ahmed <2021462@iub.edu.bd>
2023-08-31 23:24:08 +0600

    updated styles and theme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        ></link>
         <link rel="icon" href="/logo.png" />
       </Head>
       <body>
